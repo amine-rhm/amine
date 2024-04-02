99e2f9364afdc47f5a82d8ecf0295ae5c414ba78
RAHMOUNI Mohamed Amine <144079280+amine-rhm@users.noreply.github.com>
2024-04-02 20:38:29 +0200

    Update locu.js

--- server/locu.js
+++ server/locu.js
@@ -1,25 +1,51 @@
 const express=require("express");
 const app=express();
+const bodyParser =require( "body-parser");
 const { v4: uuidv4 } = require('uuid');
 const { generateJwt } = require("./jwt/genratetoken");
-const jwtSecret="3##"
-const auth = require ( "./middleware/auth");
+const cors =require("cors");
 const mysql = require('mysql');
-const bcrypt=require("bcrypt")
-const saltOrRounds=10;
+const fs =require("fs");
+const bcrypt=require("bcryptjs")
+require('dotenv').config();
 const jwt =require("jsonwebtoken");
 const { upload } = require('./middleware/fich');
-const db = mysql.createConnection({
+const path =require( "path");
+const cookieParser =require("cookie-parser");
+const helmet =require("helmet");
+app.use(cookieParser());
+app.use(helmet());
+const auth = require("./middleware/auth");
+const { error, Console } = require("console");
+const pool = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "tp02"
+  database: "newdata"
 });
 
 app.use(express.json()) ;
+app.use(cors({ origin: "http://localhost:3001", credentials: true }));
+app.use(cookieParser());
+app.use(express.json()); // req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  helmet({
+    frameguard: {
+      action: "deny",
+    },
+    xssFilter: true,
+    crossOriginResourcePolicy: {
+      policy: "cross-origin",
+    },
+  })
+);
+
+
 
 // Connexion à la base de données
-db.connect((err) => {
+pool.connect((err) => {
   if (err) {
     console.error('Erreur de connexion à la base de données:', err);
   } else {
@@ -28,28 +54,18 @@ db.connect((err) => {
 });
 
 
-// juste un exemple lors de teste
-app.get("/users",(req,res)=>{
-const sql="select * from users";
-db.query (sql,(err,data)=>{
-if (err)
-    return res.json(err)
-    return res.json(data)
-})
-})
-
-
-// cree un compte sur la platforme locu
+saltOrRounds=10;
+// Register User
 app.post("/api/v1/register", (req, res) => {
   try {
-    const lastname = req.body.lastname;
-    const firstname = req.body.firstname;
+    const nom = req.body.nom;
+    const prenom = req.body.prenom;
     const email = req.body.email; 
     const password = req.body.password; 
 
     // Vérification si l'email est déjà utilisé
-    db.query(
-      `SELECT * FROM registerr WHERE email = ?`,
+    pool.query(
+      `SELECT * FROM client WHERE email = ?`,
       [email],
       (err, result) => {
         if (err) {
@@ -67,28 +83,29 @@ app.post("/api/v1/register", (req, res) => {
             return res.json({ error: " erreur trouvé dans le hachage" });
           }
 
-          
+          const userId = uuidv4();
           // Ajout des données dans la base de données
-          db.query(
-            "INSERT INTO registerr (iduser, lastname, firstname, email, password) VALUES (?, ?, ?, ?, ?)",
-            [uuidv4(), lastname, firstname, email, hash],
+          pool.query(
+            "INSERT INTO client (iduser,nom,prenom, email, password) VALUES (?, ?, ?, ?, ?)",
+            [ userId,nom,prenom, email, hash],
             (err, result) => {
               if (err) {
                 console.error("Error inserting user into database:", err);
                 return res.json({ error: err.message }); 
               }
-          
+        
 
               // Génération du JWT
-              const token = generateJwt({ email, firstname, lastname });
+              
+               const token = generateJwt({userId , email, nom, prenom, hash });
               console.log("token generated successfully:");
               
-
               // Envoi du cookie contenant le token
               res.cookie("accessToken", token, {
                 httpOnly: true,
                 secure: true,
               }).json({ token });
+              console.log(token)
             }
           );
         });
@@ -105,8 +122,9 @@ app.post("/api/v1/register", (req, res) => {
 app.post("/api/v1/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  db.query(
-    'SELECT * FROM registerr WHERE email = ?',
+
+  pool.query(
+    'SELECT * FROM client WHERE email = ?',
     [email],
     (err, result) => {
       if (err) {
@@ -116,10 +134,15 @@ app.post("/api/v1/login", (req, res) => {
         const hashedPassword = result[0].password; 
         bcrypt.compare(password, hashedPassword, (error, response) => {
           if (response) {
-            const userId = result[0].id; 
-            const tok= jwt.sign(({userid:userId}), jwtSecret , { expiresIn: "1h" });
-            console.error(error)
-            res.json({ message: "Authentication successful", token: tok });
+            const userId = result[0].iduser;
+            const token = generateJwt({userId});
+            console.log(userId);
+
+            res.cookie("accessToken", token, {
+                httpOnly: true,
+                secure: true,
+              })
+              .json({ token });
           } else {
             console.log("token");
             res.status(401).json({ message: "Wrong password" });
@@ -145,29 +168,30 @@ app.get("/api/v1/logout", (request, response) => {
   }
 });
 
-
 //prover the auth
-app.get("/api/v1/profile", auth, async (request, response) => {
+app.get("/api/v1/verif", auth, async (req, res) => {
   try {
-    const user = await pool.query(
-      "SELECT iduser, nom, prenom, email FROM client WHERE iduser = ?",
-      [request.user.iduser]
-    );
+    const userId = req.userData.iduser;
+    
+    const user = await pool.query(" SELECT * FROM client WHERE iduser = ?", 
+    [userId]);
 
-    if (user.rows.length === 0) {
-      return response.json({ msg: "Utilisateur non trouvé" });
+    if (!user || user.length === 0) {
+      return res.json({ message: "Utilisateur non trouvé dans la base de données" });
     }
+    res.json({ message: "Token valide" });
+   console.log(userId);
 
-    response.json(user.rows[0]);
   } catch (error) {
-    console.error(error.message);
-    response.json({ msg: "Erreur serveur" });
+
+    console.error("Erreur lors de la vérification du token avec l'ID de l'utilisateur:", error);
+    res.json({ message: "Erreur server lors de la vérification du token avec l'ID de l'utilisateur" });
   }
 });
 
 
 // Verify the current user token if authenticated
-app.get("/api/v1/verify", auth, async (request, response) => {
+app.get("/api/v1/that", auth, async (request, response) => {
   try {
     response.json(true);
   } catch (error) {
@@ -177,31 +201,50 @@ app.get("/api/v1/verify", auth, async (request, response) => {
 });
 
 
-// cree une annonce sur la platforme locu
-app.post('/api/v1/new/annonce',auth, upload.array('file',3), async (req, res) => {
+
+  // dayi tla lkhdma wahi uth ni dyi pour essaiyer juste jai fixer le id de user
+// déposer une annonce 
+
+app.post('/api/v1/new/annonce', auth, upload.array('file', 5), async (req, res) => {
+
+  const userId=req.userData.userId;
+
   try {
     if (!req.files || req.files.length === 0) {
       return res.send({ message: "Aucun fichier n'a été téléchargé." });
     }
-    const image1 = req.files[0].filename;
-    const image2 = req.files[1].filename;
-    const image3 = req.files[2].filename;
-   
-    //const userId = request.user.user_id;  tayi apres kn wahi frontend
-    userId=123456;
-    const { type ,surface,adresse,prix,titre, description } = req.body;
 
-    // Validation pour voir
-    if (!titre || !description|| !surface|| !prix|| !description|| !adresse) {
-      return res.send({ message: "ces champs sont  requis." });
+    const image1 = req.files[0].filename;  
+    const image2 = req.files[1] ? req.files[1].filename : null;
+    const image3 = req.files[2] ? req.files[2].filename : null;
+    const image4 = req.files[3] ? req.files[3].filename : null;
+    const image5 = req.files[4] ? req.files[4].filename : null;
+   
+ 
+    const dateAjout = new Date();
+    const {type, surface, adresse, prix, titre, description} = req.body;
+    if (!titre || !description || !prix || !description || !adresse) {
+      return res.send({ message: "Ces champs sont requis."});
     }
-    
-// insert to database 
-    await db.query(
-      "INSERT INTO annonce (idann,type,surface,adresse,prix, titre, description, image1,image2,image3,user_id) VALUES (?,?,?,?,?,?,?,?,?,?,?)",
-      [uuidv4(),type,surface,adresse,prix, titre, description, image1,image2,image3,userId]
+    const idann = uuidv4();
+    await pool.query(
+      "INSERT INTO annonce (idann, titre, description, date_ajout, image1, image2, image3,image4,image5,iduser) VALUES (?, ?, ?,?, ?, ?, ?, ?,?,?)",
+      [idann, titre, description,dateAjout,image1, image2, image3,image4,image5,userId]
     );
-    res.send({ message: "Annonce ajoutée avec succès.", annonceId: uuidv4() , userId});
+    console.log("bien inserrer dans la table annonce")
+    // Insérer dans la table Bien si nécessaire
+    const idB = uuidv4();
+    await pool.query(
+      "INSERT INTO Bien (idB, type,surface, prix, adresse,userId, idann) VALUES (?, ?, ?, ?, ?, ?,?)",
+      [idB, type,surface, prix, adresse, userId, idann]
+      
+
+    );
+
+    console.log("bien inserrer dans la table bien")
+
+    // Retour d'informations supplémentaires
+    res.send({ message: "Annonce ajoutée avec succès.", idann});
   } catch (error) {
     console.error("Erreur lors de l'ajout de l'annonce :", error);
     res.send({ message: "Une erreur s'est produite lors de l'ajout de l'annonce." });
@@ -209,24 +252,26 @@ app.post('/api/v1/new/annonce',auth, upload.array('file',3), async (req, res) =>
 });
 
 
-// recuperere tout les annonces 
-app.get("/api/v1/all/annonces", async(req,res)=>{
-  try{
-
-  const toutes = await db.query(
- " select ( idann, type,surface,adresse,prix, titre, description,image1,image2,image3 ) from annonce ordere by annonce.idann " 
-)
-console.log("a ce niveau ya pas derreur")
-   res.json({
-    //  tab de objets 
-    totalListing : toutes.rows.length,
-    listing : toutes.rows,
-   });
-  } catch(error){
-
-      console.log(error);
-  };
-})
+// recuperere tous les annonces 
+app.get("/api/v1/all/annonce", (req, res) => {
+  try {
+    pool.query(
+      "SELECT idann, titre, description, date_ajout, image1, image2, image3, image4, image5, iduser FROM annonce ORDER BY idann",
+      (error, result) => {
+        if (error) {
+      return res.status(500).json({ message: "Erreur lors de la récupération des annonces." });
+        }
+        console.log("A ce niveau, il n'y a pas d'erreur");
+        res.json({
+          totalListing:result.length,
+          listing: result
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // recuperer une annonce avec sans id 
 app.get("/api/v1/single/annonces/:id", async (request, response) => {
@@ -251,6 +296,7 @@ app.get("/api/v1/single/annonces/:id", async (request, response) => {
     response.status(500).json({ error: "Une erreur s'est produite lors de la récupération des détails de l'annonce." });
   }
 });
+
 
 
 // recuperer une annonce avec id specifique et on inclu les info de le client 
@@ -279,11 +325,12 @@ app.get("/api/v1/single/whith info client/annonces/:id", async (request, respons
 });
 
 
-//  section recement ajouter , recuperere les dernier annonce ajouter .
+ //section recement ajouter , recuperere les dernier annonce ajouter.
+
 app.get("/api/v1/recement/annonces", (req, res) => {
   try {
-    db.query(
-      "SELECT idann, type, surface, adresse, prix, titre, description, image1, image2, image3 FROM annonce",
+    pool.query(
+      "SELECT idann, titre, description, date_ajout, image1, image2, image3, image4, image5, iduser FROM annonce",
       (error, result) => {
         if (error) {
           console.log(error);
@@ -292,11 +339,9 @@ app.get("/api/v1/recement/annonces", (req, res) => {
         }
         
         // Triez les annonces par ID 1 2 3 4 5 
-        result.rows.sort((a, b) => b.idann - a.idann);
-
-        // Sélectionnez les 12 premières annonces
-        const dDernieresAnnonces = result.rows.slice(0, 12);
-        // Envoyez les annonces sélectionnées en réponse
+        result.sort((a, b) => b.idann - a.idann);
+        // dayi labghit 3 4 lsl atan 12 aka
+        const dixDernieresAnnonces = result.slice(0, 12);
         res.json({ annonces: dixDernieresAnnonces });
       }
     );
@@ -329,13 +374,12 @@ app.get("/api/v1/basique/recherche", (req, res) => {
   );
 });
 
-
 // recherche avancé
 app.get("/api/v1/avance/recherche", (req, res) => {
   const {ville, prix,surface,meublé,type} = req.body;
 
   pool.query(
-    "SELECT bien.surface,bien.type,bien.meublé,annonce.idann, annonce.titre, annonce.description, annonce.date_ajout, annonce.image1, annonce.image2, annonce.image3, annonce.image4, annonce.image5,annonce.iduser FROM annonce JOIN bien ON bien.idann = annonce.idann WHERE bien.ville LIKE ? and prix <= ? and bien.surface=? and bien.meublé= ? and bien.type =? ",
+    "SELECT annonce.idann, annonce.titre, annonce.description, annonce.date_ajout,bien.surface,bien.type,bien.meublé, annonce.image1, annonce.image2, annonce.image3, annonce.image4, annonce.image5,annonce.iduser FROM annonce JOIN bien ON bien.idann = annonce.idann WHERE bien.ville LIKE ? and prix <= ? and bien.surface=? and bien.meublé= ? and bien.type =? ",
     [`%${ville}%`,prix,surface,meublé,type],
       (error, result) => {
           if (error) { 
@@ -351,8 +395,6 @@ app.get("/api/v1/avance/recherche", (req, res) => {
   );
 });
 
-
-// le port et message de connextion au serveur
 app.listen(3001,()=>{
 console.log("I am listen what kho ")
 
